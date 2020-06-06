27f84fdf2be184d598b9345ad3c9aa99730fb82d
deepuhifi <deepuhifi@gmail.com>
2020-06-06 12:54:15 +0530

    files added with changes

--- app.py
+++ app.py
@@ -19,13 +19,36 @@ def book():
     """""BOOK FLIGHT"""""
 
     name = request.form.get("name")
+    if len(name) == 0:
+        return render_template("error.html",message="enter an valid name")
+
     try:
         flight_id = int(request.form.get("flight_id"))
     except ValueError:
-        render_template("error.html",message="invalid flight no")
+        return render_template("error.html",message="invalid flight no")
 
     if db.execute("SELECT * FROM flights WHERE id = :id",{"id":flight_id}).rowcount == 0:
-        render_template("error.html",message = "no such flight with that id")
+        return render_template("error.html",message = "no such flight with that id")
     db.execute("INSERT INTO passengers (name, flight_id) VALUES(:name, :flight_id)",{"name": name, "flight_id":flight_id})
     db.commit()
     return render_template("success.html", message="YOu have Sucessfully booked an flight")
+
+@app.route("/flights")
+def flights():
+    """Lists all flights."""
+    flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("flights.html", flights=flights)
+
+@app.route("/flights/<int:flight_id>")
+def flight(flight_id):
+    """Lists details about a single flight."""
+
+    # Make sure flight exists.
+    flight = db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).fetchone()
+    if flight is None:
+        return render_template("error.html", message="No such flight.")
+
+    # Get all passengers.
+    passengers = db.execute("SELECT name FROM passengers WHERE flight_id = :flight_id",
+                            {"flight_id": flight_id}).fetchall()
+    return render_template("flight.html", flight=flight, passengers=passengers)
